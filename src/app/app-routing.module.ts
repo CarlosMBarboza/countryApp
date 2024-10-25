3538e9c9b09cc06529c764d1c5fe4ba89de85804
CarlosMBarboza <michaelalexander81@hotmail.com>
2024-10-25 10:46:12 -0300

    fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { HomePagesComponent } from './shared/pages/home-pages/home-pages.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomePagesComponent
+  // },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+  loadChildren:()=> import('./countries/countries.module').then(m => m.CountriesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'countries',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
